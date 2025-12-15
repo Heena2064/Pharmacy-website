27bafcab193bf93cf17ece52289ddab901516695
Heena <heena@example.com>
2025-12-15 14:45:58 +0530

    fix aos import

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,11 @@ import 'aos/dist/aos.css'
 AOS.init({
   duration: 800,
   once: true,
-});
+})
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <App />
-  </StrictMode>,
+  
 )
